0b81b452941d3b85aa28b2088a353c3fc995fa31
Moses Mwai <mosesmwaiw@gmail.com>
2023-10-03 17:31:56 +0300

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+# Moses Mwai Wahome SCT211-0029/2022
+
+digit1= input("Enter the first digit")
+digit2= input("Enter the second digit")
+
+sum = digit1 + digit2
+
+print("The sum of " +digit1+ " and "+digit2+" is "+ sum)
